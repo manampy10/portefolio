5b67a7d7e5c45381ab64e5ede338ac7236104197
manampy_abraham <abraham.dev89@gmail.com>
2025-08-21 18:38:28 +0200

    test_contrast

--- script/script.js
+++ script/script.js
@@ -38,14 +38,14 @@ function createSkillsFromJSON() {
         const card = document.createElement("div");
         card.classList.add("col-lg-4", "mt-4");
         card.innerHTML = `
-                    <div class="card skillsText">
-                        <div class="card-body">
-                            <img src="./images/${item.image}" />
-                          <h3 class="card-title mt-3">${item.title}</h3>
-                            <p class="card-text mt-3">${item.text}</p>
-                        </div>
-                    </div>
-                `;
+             <div class="card skillsText">
+             <div class="card-body">
+             <img src="./images/${item.image}" alt="${item.title} logo" />
+             <h3 class="card-title mt-3">${item.title}</h3>
+             <p class="card-text mt-3">${item.text}</p>
+         </div>
+         </div>
+`;
 
         // Append the card to the current row
         row.appendChild(card);
@@ -74,17 +74,17 @@ function createPortfolioFromJSON() {
         const card = document.createElement("div");
         card.classList.add("col-lg-4", "mt-4");
         card.innerHTML = `
-                    <div class="card portfolioContent">
-                    <img class="card-img-top" src="images/${item.image}" style="width:100%">
-                    <div class="card-body">
-                        <h3 class="card-title">${item.title}</h3>
-                        <p class="card-text">${item.text}</p>
-                        <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success">Lien</a>
-                        </div>
-                    </div>
-                </div>
-                `;
+    <div class="card portfolioContent">
+    <img class="card-img-top" src="images/${item.image}" alt="Aperçu du projet ${item.title}" style="width:100%">
+    <div class="card-body">
+      <h3 class="card-title">${item.title}</h3>
+      <p class="card-text">${item.text}</p>
+      <div class="text-center">
+        <a href="${item.link}" class="btn btn-success">Lien</a>
+      </div>
+    </div>
+  </div>
+`;
 
         // Append the card to the current row
         row.appendChild(card);
